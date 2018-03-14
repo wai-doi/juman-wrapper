6ae057982c9cb0952b891a302cad9c98145f1f77
yusuke <>
2018-03-14 23:52:37 +0900

    update README.md

--- juman_parser.rb
+++ juman_parser.rb
@@ -42,8 +42,18 @@ end
 
 
 if $0 == __FILE__
-  text = "東京は日本の首都です。"
+  text = "私はコンビニでお弁当を買った。"
   juman = JumanParser::Parser.new(text)
-  pp juman.surfaces
-  pp juman.morphemes.map(&:pos)
+
+  # 表層形の配列
+  p juman.surfaces
+  # => ["私", "は", "コンビニ", "で", "お", "弁当", "を", "買った", "。"]
+
+  # 形態素オブジェクト
+  p juman.morphemes.first
+  # => #<JumanParser::Morpheme:0x00007fd56388e5b0 @surface="私", @yomi="わたし", @midasi="私", @pos="名詞", @detail_pos="普通名詞">
+
+  # 品詞の配列
+  p juman.morphemes.map(&:pos)
+  # => ["名詞", "助詞", "名詞", "助詞", "接頭辞", "名詞", "助詞", "動詞", "特殊"]
 end
